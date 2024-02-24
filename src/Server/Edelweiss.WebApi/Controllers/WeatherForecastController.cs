e5a233037e12a89fa46ec61a4616476eb4f4d096
MinhTruongDev <104303855+MinhTruongDev@users.noreply.github.com>
2024-02-24 20:41:36 +0700

    Testing Serilog

--- src/Server/Edelweiss.WebApi/Controllers/WeatherForecastController.cs
+++ src/Server/Edelweiss.WebApi/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 namespace Edelweiss.WebApi.Controllers;
 [ApiController]
@@ -10,22 +11,55 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
-        _logger = logger;
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
+            .Build();
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        try
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+            ValidateResult(result);
+            return result;
+        }
+        catch (Exception ex)
+        {
+            Log.Logger.Verbose("Verbose: {ex}",ex);
+            Log.Logger.Debug("Debug: {ex}", ex);
+            Log.Logger.Warning("Warning: {ex}", ex);
+            Log.Logger.Information("Information: {ex}", ex);
+            Log.Logger.Error("Error: {ex}",ex);
+            Log.Logger.Fatal("Fatal: {ex}", ex);
+            Log.CloseAndFlush();
+            return new List<WeatherForecast>();
+        }
+    }
+
+    private static void ValidateResult(WeatherForecast[] result)
+    {
+        if (result.Any(a => a.Summary == "Hot" || a.TemperatureC > 30))
+            throw new Exception(GetErrorMessage());
+    }
+
+    private static string GetErrorMessage()
+    {
+        return "That shit's hot";
     }
 }
