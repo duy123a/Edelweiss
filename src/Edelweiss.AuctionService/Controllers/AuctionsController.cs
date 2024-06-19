0575927a0b2448f1f206e4de9319c1e02d74519d
duy123a <duy123a@gmail.com>
2024-06-19 09:10:42 +0700

    feat: add auctions controller

---
+++ src/Edelweiss.AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Edelweiss.AuctionService.Data;
+using Edelweiss.AuctionService.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Edelweiss.AuctionService.Controllers;
+
+[ApiController]
+[Route("api/auctions")]
+public class AuctionsController : ControllerBase
+{
+    private readonly AuctionDbContext _context;
+    private readonly IMapper _mapper;
+    public AuctionsController(AuctionDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+    {
+        var auctions = await _context.Auctions
+            .Include(x => x.Item)
+            .OrderBy(x => x.Item.Creator)
+            .ToListAsync();
+
+        return _mapper.Map<List<AuctionDto>>(auctions);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AuctionDto>> GetAunctionById(Guid id)
+    {
+        var auction = await _context.Auctions
+            .Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        return _mapper.Map<AuctionDto>(auction);
+    }
+}
