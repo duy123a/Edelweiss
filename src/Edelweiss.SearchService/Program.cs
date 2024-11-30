826020115d48b3ffc1d5846c24111656b9588c22
duy123a <duy123a@gmail.com>
2024-11-30 20:28:32 +0700

    feat: add policy for calling auction service

--- src/Edelweiss.SearchService/Program.cs
+++ src/Edelweiss.SearchService/Program.cs
@@ -1,12 +1,17 @@
 using Edelweiss.SearchService.Data;
 using Edelweiss.SearchService.Services;
+using Polly;
+using Polly.Extensions.Http;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+builder.Services
+    .AddHttpClient<AuctionSvcHttpClient>()
+    .AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -16,14 +21,27 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitializer.InitDb(app);
-}
-catch (Exception e)
-{
-    Console.WriteLine(e);
-    throw;
-}
+    // Will block the app run due to policy, so should put it here
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        throw;
+    }
+});
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+{
+    return HttpPolicyExtensions
+        // Handle transient error, like connection timeout (error that likely won't be an error in the future)
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
+}
